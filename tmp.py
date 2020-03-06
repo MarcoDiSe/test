dd805e42fa0b21c261382ce7eb4f1fd180d29ef2
Marco Di Seglio <59996134+MarcoDiSe@users.noreply.github.com>
2020-03-06 13:28:20 +0100

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -5,7 +5,8 @@ Created on Fri Mar  6 13:12:33 2020
 @author: andrea
 """
 import numpy as np
-import tensorflow as giampiero
+import matplotlib as plt
 while True:
     print('tmp')
-
+    
+print('tmp')
